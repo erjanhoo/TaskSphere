ff71f83121d4ef50dcd21f1ba7d20d512df3b124
Your Name <erjansebat@gmail.com>
2026-01-31 23:26:12 +0600

    minor changes

--- TaskSphere/user/urls.py
+++ TaskSphere/user/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from .views import (
     UserRegistrationView,
-    UserRegistrationOTPVerificationView,
+    # UserRegistrationOTPVerificationView,
     UserLoginView,
-    UserLoginOTPVerificationView,
+    # UserLoginOTPVerificationView,
     UserResendOTPView,
     UserForgotPasswordView,
-    UserForgotPasswordOTPVerificationView,
+    # UserForgotPasswordOTPVerificationView,
     UserLogoutView,
     UserProfileView,
     UserBadgesView,
@@ -18,16 +18,16 @@ from .views import (
 urlpatterns = [
     # Registration
     path('register/', UserRegistrationView.as_view(), name='user-register'),
-    path('register/verify-otp/', UserRegistrationOTPVerificationView.as_view(), name='register-verify-otp'),
+    # path('register/verify-otp/', UserRegistrationOTPVerificationView.as_view(), name='register-verify-otp'),
     path('resend-otp/', UserResendOTPView.as_view(), name='resend-otp'),
     
     # Login
     path('login/', UserLoginView.as_view(), name='user-login'),
-    path('login/verify-otp/', UserLoginOTPVerificationView.as_view(), name='login-verify-otp'),
+    # path('login/verify-otp/', UserLoginOTPVerificationView.as_view(), name='login-verify-otp'),
     
     # Password Reset
     path('forgot-password/', UserForgotPasswordView.as_view(), name='forgot-password'),
-    path('forgot-password/verify/', UserForgotPasswordOTPVerificationView.as_view(), name='forgot-password-verify'),
+    # path('forgot-password/verify/', UserForgotPasswordOTPVerificationView.as_view(), name='forgot-password-verify'),
     
     # Logout
     path('logout/', UserLogoutView.as_view(), name='user-logout'),
