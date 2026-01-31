b457164ae7787df8ff06ae857852ebf76dc76504
Your Name <erjansebat@gmail.com>
2026-01-31 20:22:11 +0600

    Email backends added to settings

--- TaskSphere/TaskSphere/settings.py
+++ TaskSphere/TaskSphere/settings.py
@@ -137,3 +137,14 @@ SIMPLE_JWT = {
 }
 
 CORS_ALLOW_ALL_ORIGINS = True 
+
+
+
+# === EMAIL SETTINGS (Добавь это!) ===
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = os.getenv('EMAIL_HOST', 'smtp.gmail.com')
+EMAIL_PORT = int(os.getenv('EMAIL_PORT', 587))
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', 'True') == 'True'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
